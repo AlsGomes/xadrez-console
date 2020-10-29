34c33a4c49697cc3b50788b371b2b90fba6e2fd1
Alisson Gomes <44034024+AlsGomes@users.noreply.github.com>
2020-10-29 19:12:54 -0300

    Beginning the implementation of Tabuleiro

--- xadrez-console/Tabuleiro/Posicao.cs
+++ xadrez-console/Tabuleiro/Posicao.cs
@@ -1,4 +1,4 @@
-﻿namespace Tabuleiro
+﻿namespace xadrez_console.tabuleiro
 {
     class Posicao
     {
---
+++ xadrez-console/tabuleiro/Peca.cs
@@ -0,0 +1,23 @@
+﻿using xadrez_console.tabuleiro.enums;
+
+namespace xadrez_console.tabuleiro
+{
+    class Peca
+    {
+        public Posicao Posicao { get; set; }
+
+        public Cor Cor { get; protected set; }
+
+        public int QntdMovimentos { get; protected set; }
+
+        public Tabuleiro Tabuleiro { get; protected set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tabuleiro)
+        {
+            Posicao = posicao;
+            Cor = cor;
+            QntdMovimentos = 0;
+            Tabuleiro = tabuleiro;
+        }
+    }
+}
